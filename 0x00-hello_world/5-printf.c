cf1f8d6b39dd0df4805f0c54bebc020143dc7822
zaacode <zdzaacode@gmail.com>
2023-10-09 13:15:55 -0700

    new

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - This is the main function
+ * Return: returns Zero
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
